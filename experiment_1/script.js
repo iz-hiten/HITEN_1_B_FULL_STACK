4fa40a7bfc819b0545022b5139ac3d85539620c1
iz-hiten <147185573+iz-hiten@users.noreply.github.com>
2025-08-25 12:40:27 +0530

    Create script.js

---
+++ experiment_1/script.js
@@ -0,0 +1,22 @@
+console.log("Hello World!");
+
+function greetUser() {
+    console.log("Welcome to JavaScript!");
+}
+greetUser();
+
+function addNumbers(a, b) {
+    return a + b;
+}
+let result = addNumbers(5, 3);
+console.log("5 + 3 =", result);
+
+function printName(name) {
+    console.log("My name is " + name);
+}
+printName("Hiten");
+function sq(num){
+    return num*num;
+}
+console.log(sq(4))
+
